1d9f276cdfa586fca332f5b1b6feca08fbe5f520
Muhamad Lotfy <mhesham.fcis@gmail.com>
2012-04-14 21:09:26 +0200

    Prompt user to enter StarCraft and BAWAPI path.
    Signed-off-by: Muhamad Lotfy <mhesham.fcis@gmail.com>

--- Setup.bat
+++ Setup.bat
@@ -1,25 +1,23 @@
-@Echo off
+@echo off
 
+:: Clone user repos
 set /p user=Enter your GitHub username:
 echo Cloning RTS AI Research repos ...
 git clone https://%user%@github.com/%user%/IStrategizer.git ..\IStrategizer
 git clone https://%user%@github.com/%user%/CaseVisualizer.git ..\CaseVisualizer
 git clone https://%user%@github.com/%user%/Serialization.git ..\Serialization
 
+:: Get BWAPI framework path
 set bwapi=..\bwapi 3.7.2
+set /p bwapi=Enter BWAPI path: %=%
+echo BWAPI path: %bwapi%
+:: Get StarCraft game path
 set starcraft=..\starcraft
-:: set bwapi_default=
-:: set starcraft_default=
+set /p starcraft=Enter StarCraft path: %=%
+echo StarCraft path: %starcraft%
 
-:: Todo: Ask user for bwapi and starcraft pathes, if user press enter and the provided path was empty assume a default path and show it to him
-:: else use the provided path
-:: Echo Enter BWAPI path:
-:: Echo Etner StarCraft path:
-:: if provided pathes are empty, then use defaults, else use provided ones
-
-Echo Setting up IStrategizer environment ...
-
-Echo Copying BWAPI files ...
+echo Setting up IStrategizer environment ...
+echo Copying BWAPI files ...
 xcopy "%bwapi%\include" "..\IStrategizer\Build\IStrategizerBWAPIClient\include" /Y /I /E /Q
 xcopy "%bwapi%\lib" "..\IStrategizer\Build\IStrategizerBWAPIClient\lib" /Y /I /E /Q
 xcopy "%bwapi%\windows\*.*" "..\IStrategizer\Build\Debug\*.*" /Y /I /E /Q
@@ -27,12 +25,14 @@ xcopy "%bwapi%\starcraft\*.*" "%starcraft%\*.*" /Y /I /E /Q
 xcopy "%bwapi%\chaoslauncher\*.*" "%starcraft%\chaoslauncher\*.*" /Y /I /E /Q
 xcopy "bwapi.ini" "%starcraft%\bwapi-data" /Y /Q
 
-Echo Compiling Serialization Libraries
+echo Compiling Serialization Libraries
 cd ..\Serialization
 msbuild Serialization.sln /p:configuration=debug /p:platform=win32 /t:clean
 msbuild Serialization.sln /p:configuration=debug /p:platform=win32
+cd ..
 
-Echo Done!
+echo Congratulations, setup is done!
+
 pause
 set bwapi=
 set starcraft=
